e828496c62d43bb94383f35e219bd3c8bd40782a
kruvv <kruvv@mail.ru>
2026-04-07 13:31:49 +0300

    Clean server

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,3 @@
-// import { Configuration, OpenAIApi } from 'openai';
 import OpenAI from "openai";
 import express from 'express';
 import bodyParser from 'body-parser';
@@ -15,14 +14,6 @@ app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)
 });
 
-// const config = new Configuration({
-//     // organization: process.env.ORGANIZATION_NAME,
-//     apiKey: process.env.OPEN_AI_API_KEY
-// });
-
-// console.log('apiKey: ', process.env.OPENAI_API_KEY)
-// console.log('PORT: ', process.env.PORT)
-
 const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY
 });
@@ -34,10 +25,6 @@ app.post('/', async(req, res) => {
         model: process.env.MODEL_CHAT,
         input: 'write a haiku about ai on russian',
         store: true
-        // messages: [{
-        //     role: 'system',
-        //     content: 'You are a EbereGPT. You can help with program tasks'
-        // }, ...chats]
     });
 
     console.log('response: ', response)
